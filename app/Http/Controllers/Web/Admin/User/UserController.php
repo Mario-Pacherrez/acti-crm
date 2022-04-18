59cfec4a67ec50b9a6a3c56b2a07df6a259b5503
Mario-Pacherrez <mario.pacherrez@urp.edu.pe>
2022-04-18 10:45:23 -0500

    Agregando paginación.

--- app/Http/Controllers/Web/Admin/User/UserController.php
+++ app/Http/Controllers/Web/Admin/User/UserController.php
@@ -19,7 +19,7 @@ class UserController extends Controller
 {
     public function index()
     {
-        $users = User::with('roles')->get();
+        $users = User::with('roles')->paginate(5);
         return view('admin.users.index', compact('users'));
     }
 
